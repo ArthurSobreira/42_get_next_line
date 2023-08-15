849a5c5901625472cc16a547167c9057843719c4
Arthur <tutusobreira@gmail.com>
2023-08-15 20:38:15 -0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -11,83 +11,103 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
+
+static char	*ft_get_lines(int fd, char *memory_str);
+static char	*ft_actual_line(char *memory_str, char *line);
+static char	*ft_get_rest(char *memory_str);
 
 char	*get_next_line(int fd)
 {
-	static char	*rest;
-	char		*buffer;
+	static char	*memory_str;
 	char		*line;
-	int			byte_nb;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
-	
-	if (rest == NULL)
+	memory_str = ft_get_lines(fd, memory_str);
+	if (memory_str == NULL)
+		return (NULL);
+	line = NULL;
+	line = ft_actual_line(memory_str, line);
+	memory_str = ft_get_rest(memory_str);
+	return (line);
+}
+
+static char	*ft_get_lines(int fd, char *memory_str)
+{
+	char	*tmp;
+	int		tmp_len;
+
+	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (tmp == NULL)
+		return (NULL);
+	tmp_len = -1;
+	while ((ft_strchr(memory_str, '\n')) == NULL && tmp_len != 0)
 	{
-		buffer = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-		byte_nb = 1;
-		while (byte_nb > 0)
+		tmp_len = read(fd, tmp, BUFFER_SIZE);
+		if (tmp_len < 0)
 		{
-			byte_nb = read(fd, buffer, BUFFER_SIZE);
-			if (!(byte_nb <= 0))
-			{
-				buffer[byte_nb] = '\0';
-				rest = buffer;
-			}
+			free(tmp);
+			return (NULL);
 		}
+		tmp[tmp_len] = '\0';
+		memory_str = ft_strjoin(memory_str, tmp);
 	}
+	free(tmp);
+	return (memory_str);
+}
 
-	t_list *list;
-    t_char *current_node;
+static char	*ft_actual_line(char *memory_str, char *line)
+{
+	int	count;
 
-	list = ft_new_list();
-	current_node = NULL;
-    
-    while (*rest != '\n')
-    {
-        if (list->head == NULL)
-        {
-            list->head = ft_new_node(*rest);
-            current_node = list->head;
-        }
-        else
-        {
-            current_node->next = ft_new_node(*rest);
-            current_node = current_node->next;
-        }
-        rest++;
-    }
-    
-    if (*rest == '\n')
+	if (memory_str[0] == '\0')
+		return (NULL);
+	count = 0;
+	while (memory_str[count] != '\0' && memory_str[count] != '\n')
+		count++;
+	if (memory_str[count] == '\0')
+		line = malloc((count + 1) * sizeof(char));
+	else if (memory_str[count] == '\n')
+		line = malloc((count + 2) * sizeof(char));
+	if (line == NULL)
+		return (NULL);
+	count = 0;
+	while (memory_str[count] != '\0' && memory_str[count] != '\n')
 	{
-        rest++;
+		line[count] = memory_str[count];
+		count++;
 	}
-	
-	/* int node_count = 0;
-    current_node = list->head;
-    while (current_node != NULL)
-    {
-        node_count++;
-		//printf("%c", current_node->act_char);
-        current_node = current_node->next;
-    }
-	printf("%d\n", node_count); */
-    // line = (char *)malloc((node_count + 1) * sizeof(char));
-    // if (line == NULL)
-    //     return (NULL);
+	if (memory_str[count] == '\n')
+		line[count++] = '\n';
+	line[count] = '\0';
+	return (line);
+}
 
-    // current_node = list->head;
-    // int i = 0;
-    // while (current_node != NULL)
-    // {
-    //     line[i++] = current_node->act_char;
-    //     t_char *temp = current_node;
-    //     current_node = current_node->next;
-    //     free(temp);
-    // }
-    // line[i] = '\0';
-    // list->head = NULL;
+static char	*ft_get_rest(char *memory_str)
+{
+	char	*tmp;
+	size_t	count;
+	size_t	count_tmp;
 
-    // return (line);
+	count = 0;
+	while (memory_str[count] != '\n' && memory_str[count] != '\0')
+		count++;
+	count_tmp = count;
+	if (memory_str[count_tmp] == '\0' || memory_str[0] == '\0')
+	{
+		free(memory_str);
+		return (NULL);
+	}
+	tmp = malloc((ft_strlen(memory_str) - count_tmp) * sizeof(char));
+	if (tmp == NULL)
+		return (NULL);
+	count = 0;
+	while (memory_str[++count_tmp] != '\0')
+	{
+		tmp[count] = memory_str[count_tmp];
+		count++;
+	}
+	tmp[count] = '\0';
+	free(memory_str);
+	return (tmp);
 }
