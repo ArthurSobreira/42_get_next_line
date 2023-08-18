3c951aa3fe6355ce52e0d85cc95df3e45e00c695
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-18 18:19:52 -0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,130 +12,90 @@
 
 #include "get_next_line.h"
 
+static char	*ft_fill_buffer(int fd, char *total_buffer);
 static char	*ft_get_current_line(char *total_buffer);
 static char	*ft_get_buffer_rest(char *total_buffer);
-//static char	*ft_fill_buffer(int fd, char *total_buffer);
 
 char	*get_next_line(int fd)
 {
 	static char	*total_buffer;
-	char		*temp_buffer;
 	char		*current_line;
-	int			byte_nb;
 
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
-	temp_buffer = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-	if (temp_buffer == NULL)
+	total_buffer = ft_fill_buffer(fd, total_buffer);
+	if (total_buffer == NULL)
 		return (NULL);
-	byte_nb = -1;
-	while (!(ft_strchr(total_buffer, NEW_LINE)) && (byte_nb != 0))
-	{	
-		byte_nb = read(fd, temp_buffer, BUFFER_SIZE);
-		if (byte_nb < 0)
-			return (free(temp_buffer), NULL);
-		temp_buffer[byte_nb] = NULL_BYTE;
-		total_buffer = ft_strjoin(total_buffer, temp_buffer);
-	}
-	free(temp_buffer);
 	current_line = NULL;
 	current_line = ft_get_current_line(total_buffer);
 	total_buffer = ft_get_buffer_rest(total_buffer);
 	return (current_line);
 }
 
-// char	*get_next_line(int fd)
-// {
-// 	static char	*total_buffer;
-// 	char		*current_line;
+static char	*ft_fill_buffer(int fd, char *total_buffer)
+{
+	char	*temp_buffer;
+	int		byte_nb;
 
-// 	if (BUFFER_SIZE <= 0 || fd < 0)
-// 		return (NULL);
-// 	total_buffer = ft_fill_buffer(fd, total_buffer);
-// 	if (total_buffer == NULL)
-// 		return (NULL);
-// 	current_line = NULL;
-// 	current_line = ft_get_current_line(total_buffer);
-// 	total_buffer = ft_get_buffer_rest(total_buffer);
-// 	return (current_line);
-// }
+	temp_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (temp_buffer == NULL)
+		return (NULL);
+	byte_nb = 1;
+	while (!(ft_strchr(total_buffer, '\n')) && (byte_nb != 0))
+	{
+		byte_nb = read(fd, temp_buffer, BUFFER_SIZE);
+		if (byte_nb < 0)
+		{
+			free(temp_buffer);
+			return (NULL);
+		}
+		temp_buffer[byte_nb] = '\0';
+		total_buffer = ft_strjoin(total_buffer, temp_buffer);
+	}
+	free(temp_buffer);
+	return (total_buffer);
+}
 
 static char	*ft_get_current_line(char *total_buffer)
 {
-	char		*line;
-	int			count;
+	char	*current_line;
+	char	*newline_position;
+	size_t	line_len;
 
-	if (total_buffer[0] == NULL_BYTE)
+	if (total_buffer[0] == '\0')
 		return (NULL);
-	count = 0;
-	while ((total_buffer[count] != NULL_BYTE) && \
-			(total_buffer[count] != NEW_LINE))
-		count++;
-	if (total_buffer[count] == NEW_LINE)
-		line = malloc((count + 2) * sizeof(char));
-	else if (total_buffer[count] == NULL_BYTE)
-		line = malloc((count + 1) * sizeof(char));
-	count = 0;
-	while ((total_buffer[count] != NULL_BYTE) && \
-			(total_buffer[count] != NEW_LINE))
+	newline_position = ft_strchr(total_buffer, '\n');
+	if (newline_position != NULL)
 	{
-		line[count] = total_buffer[count];
-		count++;
+		line_len = newline_position - total_buffer;
 	}
-	if (total_buffer[count] == NEW_LINE)
-		line[count++] = NEW_LINE;
-	line[count] = NULL_BYTE;
-	return (line);
+	else
+	{
+		line_len = ft_strlen(total_buffer);
+	}
+	current_line = malloc((line_len + 2) * sizeof(char));
+	if (current_line == NULL)
+		return (NULL);
+	current_line = ft_strncpy(current_line, total_buffer, line_len);
+	if (newline_position != NULL)
+	{
+		current_line[line_len++] = '\n';
+	}
+	current_line[line_len] = '\0';
+	return (current_line);
 }
 
 static char	*ft_get_buffer_rest(char *total_buffer)
 {
 	char	*rest;
-	size_t	count;
-	size_t	count_buffer;
+	char	*newline_position;
 
-	count = 0;
-	while ((total_buffer[count] != NEW_LINE) && \
-			(total_buffer[count] != NULL_BYTE))
-		count++;
-	count_buffer = count;
-	if ((total_buffer[count_buffer] == NULL_BYTE) || \
-		(total_buffer[0] == NULL_BYTE))
+	rest = NULL;
+	newline_position = ft_strchr(total_buffer, '\n');
+	if (newline_position != NULL)
 	{
-		free(total_buffer);
-		return (NULL);
+		rest = ft_strdup(newline_position + 1);
 	}
-	rest = malloc((ft_strlen(total_buffer) - count_buffer) * sizeof(char));
-	if (rest == NULL)
-		return (NULL);
-	count = 0;
-	while (total_buffer[++count_buffer] != NULL_BYTE)
-		rest[count++] = total_buffer[count_buffer];
-	rest[count] = NULL_BYTE;
 	free(total_buffer);
 	return (rest);
 }
-
-// static char	*ft_fill_buffer(int fd, char *total_buffer)
-// {
-// 	char	*temp_buffer;
-// 	int		buffer_len;
-
-// 	temp_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-// 	if (temp_buffer == NULL)
-// 		return (NULL);
-// 	buffer_len = -1;
-// 	while (!(ft_strchr(total_buffer, NEW_LINE)) && (buffer_len != 0))
-// 	{
-// 		buffer_len = read(fd, temp_buffer, BUFFER_SIZE);
-// 		if (buffer_len < 0)
-// 		{
-// 			free(temp_buffer);
-// 			return (NULL);
-// 		}
-// 		temp_buffer[buffer_len] = NULL_BYTE;
-// 		total_buffer = ft_strjoin(total_buffer, temp_buffer);
-// 	}
-// 	free(temp_buffer);
-// 	return (total_buffer);
-// }
