091765e6cb964917c4fcdf531adcd6e1fe95a418
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-18 18:27:12 -0300

    Update bonus header

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 11:32:49 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/08/17 16:12:59 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+// Functions Prototypes
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
