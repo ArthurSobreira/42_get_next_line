2fe4a69ea887f0ff06cef1f54526e837b7450f2f
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-22 14:21:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 10:41:02 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/17 16:00:31 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/22 14:20:18 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "./include/get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 
 int	main(void)
 {
-	const char	*file_name = "test2.txt";
+	const char	*file_name = "test.txt";
 	char		*c_line;
 	int			file_descriptor;
 	int			counter;
@@ -27,10 +27,12 @@ int	main(void)
 		printf("Error opening file %s\n", file_name);
 		return (1);
 	}
+	counter = 1;
 	while ((c_line = get_next_line(file_descriptor)) != NULL)
 	{
 		printf("Line %i -> %s", counter++, c_line);
 		free(c_line);
+		sleep(1);
 	}
 	close(file_descriptor);
 	return (0);
